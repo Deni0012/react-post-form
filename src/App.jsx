003ecb3e684304c471fde760a671b2819f8eadc3
Deni0012 <deni.dg00@icloud.com>
2025-04-14 15:52:01 +0200

    feat: add bootstrap and created content

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       event.target.type === 'checkbox' ?
         event.target.checked : event.target.value;
 
-    setFormData((formPosts) => ({
+    setFormPosts((formPosts) => ({
       ...formPosts,
       [event.target.name]: value,
     }));
@@ -32,6 +32,51 @@ function App() {
 
   return (
     <>
+      <div className='container'>
+        <form onSubmit={savePost}>
+          <div className="mb-3">
+            <label className="form-label">Autore</label>
+            <input type="text"
+              name='author'
+              value={formPosts.author}
+              onChange={handleFormPosts}
+              placeholder='Inserisci autore'
+              className="form-control" />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Titolo</label>
+            <input type="text"
+              name='title'
+              value={formPosts.title}
+              onChange={handleFormPosts}
+              placeholder='Inserisci titolo'
+              className="form-control" />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">Testo</label>
+            <input type="text"
+              name='body'
+              value={formPosts.body}
+              onChange={handleFormPosts}
+              placeholder='Inserisci testo'
+              className="form-control" />
+          </div>
+
+          <div className="mb-3 form-check">
+            <input type="checkbox"
+              name='public'
+              checked={formPosts.public}
+              onChange={handleFormPosts}
+              id='public'
+              className="form-check-input" />
+            <label className="form-check-label">Disponibile</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Invia</button>
+        </form>
+      </div>
+
 
     </>
   )
