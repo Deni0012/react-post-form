de6e09d4d576f7add80d01a20944e1207b89feae
Deni0012 <deni.dg00@icloud.com>
2025-04-14 15:33:33 +0200

    feat: created const and function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,31 @@ import axios from 'axios';
 
 function App() {
 
+  const [formPosts, setFormPosts] = useState({
+    author: '',
+    title: '',
+    body: '',
+    public: false
+  })
+
+  function handleFormPosts(event) {
+    const value =
+      event.target.type === 'checkbox' ?
+        event.target.checked : event.target.value;
+
+    setFormData((formPosts) => ({
+      ...formPosts,
+      [event.target.name]: value,
+    }));
+  };
+
+  function savePost(event) {
+    event.preventDefault();
+
+    console.log(formPosts);
+
+  }
+
 
   return (
     <>
